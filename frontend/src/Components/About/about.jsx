e5b2219c6b04fd9a022f8d9cb6a6dc1025c3726f
RobinDhami <rozendhami69@gmail.com>
2024-05-07 14:43:02 +0545

    about

--- frontend/src/Components/About/about.jsx
+++ frontend/src/Components/About/about.jsx
@@ -1,20 +1,38 @@
 
-import image from "../../assets/trust.png"
-const about = () => {
-  return (
-    <div className="p-10  relative  my-[15%] ">
-      <div className=" py-8 z-20">
-      <img  src={image}>
-      </img>
-      </div>
-      <div className=" px-12 ">
-      <h1 className=" font-black text-lg ">
-      What is Neem Karoli trust 
-      </h1>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faGlobe, faUsers } from '@fortawesome/free-solid-svg-icons';
 
+const About = () => {
+  return (
+    <section className="bg-gray-100 py-[20%]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <div className="lg:w-1/2 lg:pr-8">
+            <h2 className="text-3xl font-bold text-gray-800 mb-4">About Neem Karoli Trust</h2>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              The Neem Karoli Trust is a non-profit organization dedicated to promoting peace, love, and service to humanity and the planet. Our mission is inspired by the teachings of the revered spiritual master Neem Karoli Maharaj ji.
+            </p>
+          </div>
+          <div className="lg:w-1/2 lg:pl-8">
+            <div className="flex justify-around mt-8 lg:mt-0">
+              <div className="text-center">
+                <FontAwesomeIcon icon={faHeart} className="text-5xl text-red-500 mb-2" />
+                <p className="text-lg text-gray-800">Promoting Peace & Love</p>
+              </div>
+              <div className="text-center">
+                <FontAwesomeIcon icon={faGlobe} className="text-5xl text-green-500 mb-2" />
+                <p className="text-lg text-gray-800">Serving Humanity & the Planet</p>
+              </div>
+              <div className="text-center">
+                <FontAwesomeIcon icon={faUsers} className="text-5xl text-blue-500 mb-2" />
+                <p className="text-lg text-gray-800">Fostering Unity & Community</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    </section>
+  );
 }
 
-export default about
+export default About;
